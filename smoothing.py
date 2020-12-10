d943f4f2d4c3d525115d6535747906ddf3d22edc
xiyori <foma@shipilov.ru>
2020-12-10 09:17:35 +0300

    Update smoothing.py

--- smoothing.py
+++ smoothing.py
@@ -2,12 +2,9 @@ import cv2
 import average as method
 
 # cap = cv2.VideoCapture("test1.mp4")
-# cap = cv2.VideoCapture("test2.MOD")
-# cap = cv2.VideoCapture("test3.MOD")
-# cap = cv2.VideoCapture("test4.MOD")
-cap = cv2.VideoCapture("test5.MOV")
+# cap = cv2.VideoCapture(0)
 
-method.stabilize(cap, 200)
+method.stabilize(cap, 200)  # smoothing amount, larger value - lower frequency
 
 cap.release()
 cv2.destroyAllWindows()
